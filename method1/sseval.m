50e5e26249f8b5b634584dd7a52cba8ee9e94189
Janfliegt <49253507+Janfliegt@users.noreply.github.com>
2021-06-11 19:41:49 +0800

    Delete method1 directory

--- method1/sseval.m
+++
@@ -1,7 +0,0 @@
-function sse = sseval(x, xdata, ydata)
-% y = A * exp(-B*x + C)
-% https://ch.mathworks.com/help/matlab/math/example-curve-fitting-via-optimization.html
-A = x(1);
-B = x(2);
-C = x(3);
-sse = sum((ydata - A * exp(-B*xdata + C)).^2);
